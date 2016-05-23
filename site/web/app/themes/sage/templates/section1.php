56e869010dbeec4c06ce2cd55f4d62602a6ca08b
Jaron Call <jaroninthesis@gmail.com>
2016-05-23 13:04:15 -0600

    Changed Testimonials text

--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -11,6 +11,7 @@
 <!-- Add Praise Text -->
 <div class="praise-text">
   <div style="text-align: center;">Testimonials</div>
+  <div style="text-align: center;"><em>See what a few previous clients had to say..</em></div>
 </div>
 
 <!-- praises.svg -->
