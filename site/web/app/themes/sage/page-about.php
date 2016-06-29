5d9af6b97dbe62267f5cf808c302a803675ea361
Jaron Call <jaroninthesis@gmail.com>
2016-06-28 19:02:26 -0600

    Added subtext to projects page

--- site/web/app/themes/sage/page-about.php
+++ site/web/app/themes/sage/page-about.php
@@ -18,11 +18,11 @@
 <!-- Description Text -->
   <div class="col-md-6">
     <div class="about-copy text-center">
-      <div style="text-align: center;"><em>"I am a <strong>website</strong> and <strong>motion graphics designer</em></strong>,</div>
+      <div style="text-align: center;"><em>"I am a <!--<strong>website</strong> and --><strong>motion graphics designer</em></strong>,</div>
       <div style="text-aling: center;"><em>based out of <strong>Salt Lake City, Utah.</strong></em></div>
       <div style="text-aling: center;"><em>I've worked with a variety of companies,</em></div>
-      <div style="text-aling: center;"><em>ranging from <strong>Adobe Systems </strong>to small start-ups</em>.</div>
-      <div style="text-align: center;"><em>I do <strong>creative design</strong> for <strong>Jackalope Media</strong>.</em></div>
+      <div style="text-aling: center;"><em>ranging from <strong>Adobe Systems </strong>to YouTube vlogs</em>.</div>
+      <div style="text-align: center;"><em>I also do <strong>creative design</strong> for <strong>Jackalope Media</strong>.</em></div>
       <div style="text-align: center;">
         <div><em>And am the Founder of <strong>Brake The Cycle</strong>, a local Utah Organization,</em></div>
         <div><em>in support of suicide awareness".</em></div>
